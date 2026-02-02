8175f0a6cf57ff9866515c5cb6454d2a72a11f64
vanshika parashar <parasharvanshika2005@gmail.com>
2026-02-02 15:29:31 +0530

    2feb task added

--- expressjs_backend/index.js
+++ expressjs_backend/index.js
@@ -3,32 +3,50 @@ import express from "express";
 const app = express();
 
 const PORT = 8000;
+app.use(express.json());
 
 const students = [
-    { id:1, name: "john", branch: "CSE" },
-    { id:2, name: "jane", branch: "ECE" },
-    { id:3, name: "doe", branch: "MECH" },
+    { id: 1, name: "john", branch: "CSE" },
+    { id: 2, name: "jane", branch: "ECE" },
+    { id: 3, name: "doe", branch: "MECH" },
 ];
 
 app.get("/", (req, res) => {
     res.send("Welcome to Express page");
 });
 
-app.get("/user", (req, res) => {
-    res.send("Welcome to User page");
+app.get("/students/:id", (req, res) => {
+    const id = req.params.id;
+
+    const student = students.find(
+        (student) => student.id == id
+    );
+    if (!student) {
+        return res.status(404).json({ message: "Student not found" });
+    }
+    res.json(student);
 });
 
 app.get("/students", (req, res) => {
-    res.json(students);
+    const branch = req.query.branch;
+    if (!branch) {
+        return res.json(students); 
+    }
+    const foundStudents = students.filter(
+        (student) => student.branch === branch
+    );
+    res.json(foundStudents);
 });
+app.post("/students/register",(req,res)=>{
+    const data=req.body
+    console.log("(((",req.body)
+    if(!data){
+        return res.status(400).send("Please provide student data")
+    }
+    students.push(data);
+    res.json(students);
 
-app.get("/students/:id", (req, res) => {
-    res.send("")
-});   
 
-app.get("/students/search", (req, res) => {
-    const searchQuery = req.query;
-    console.log("Search Query:", searchQuery);
 });
 
 app.listen(PORT, () => {
